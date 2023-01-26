e8b0240307122903bcf095516e81f4c75f4c5e50
Don Hamilton III <donhamiltoniii@gmail.com>
2023-01-26 18:23:54 -0500

    Add home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,56 @@
+import { pageData } from "@/data/home-page";
+
 export default function Home() {
-  return <main>Yay!</main>;
+  const { sectionOne, sectionTwo, sectionThree } = pageData;
+
+  return (
+    <main>
+      <section>
+        <h2>{sectionOne.title}</h2>
+        <ul>
+          {sectionOne.list.map((item: string) => (
+            <li key={Math.random()}>{item}</li>
+          ))}
+        </ul>
+      </section>
+      <section>
+        <h2>{sectionTwo.title}</h2>
+        <ol>
+          {sectionTwo.list.map((item: string) => (
+            <li key={Math.random()}>{item}</li>
+          ))}
+        </ol>
+      </section>
+      <section>
+        <h2>{sectionThree.title}</h2>
+
+        <p>{sectionThree.description}</p>
+
+        {sectionThree.lists.map(
+          (topList: {
+            title: string;
+            foodLists: {
+              title: string;
+              list: string[];
+            }[];
+          }) => (
+            <section key={Math.random()}>
+              <h3>{topList.title}</h3>
+
+              {topList.foodLists.map(
+                (list: { title: string; list: string[] }) => (
+                  <ul key={Math.random()}>
+                    <h4>{list.title}</h4>
+                    {list.list.map((item: string) => (
+                      <li key={Math.random()}>{item}</li>
+                    ))}
+                  </ul>
+                )
+              )}
+            </section>
+          )
+        )}
+      </section>
+    </main>
+  );
 }
---
+++ src/data/home-page.ts
@@ -0,0 +1,102 @@
+export const pageData = {
+  sectionOne: {
+    title: "Goal",
+    list: [
+      "Eat healthier",
+      "Lose weight",
+      "Remove choices for food so eating right is easier",
+      "Start intermittent fasting",
+      "Get Damien eating veggies",
+    ],
+  },
+  sectionTwo: {
+    title: "Daily Practices",
+    list: [
+      `Drink water - Your body is made of water. Water is literally the thing that makes life. Drink. Water. Not soda or juice or "things that have water in it". Treat anything that isn't water as a treat. (Except the things to never drink, listed later) I would say to drink between half a gallon and a gallon a day but I understand you have a thing that sort of prevents that. But drink as much as you can everyday.`,
+      `Eat in a window - This one will take time, so do your best until you can do this and stick to it everyday. Try to eat in a 10 hour window everyday. When you start and stop is entirely up to you. There is no "best" time of the day to eat. But giving your body a solid fasting window each day is important. This means pick a time to start and stop eating and make that your window everyday.`,
+      `Don't count calories - This one is more mental than anything and I need you to trust me on this. It is medically proven that there are very few benefits to regulating calorie intake when it comes to weight loss. It's way more important that the calories you do consume serve the purpose of providing your body with what it needs.`,
+      `Snack - Snacks are actually a really good way to curb your appetite and keep you on track, as long as those snacks fit into your diet. I will give you a list of easy snacks like nuts or jerky (READ THE LABEL! Not all jerky is created equal) and other snacks that will take a little more time but are worth it.`,
+    ],
+  },
+  sectionThree: {
+    title: "Food (In General)",
+    description:
+      "Next here is some general stuff that you should eat, can eat moderately, or should not ever ever ever never ever eat.",
+    lists: [
+      {
+        title: "DO Eat",
+        foodLists: [
+          {
+            title: "Healthy Fats",
+            list: [
+              "Avocados",
+              "Eggs",
+              "Unsalted Butter",
+              "Bacon",
+              "SOME dairy - just don't overdo it",
+              "Olive Oil",
+              "Nuts and seeds",
+              "Nut and seed butters",
+              "Olives",
+            ],
+          },
+          {
+            title: "Healthy Protein",
+            list: [
+              "Chicken (pasture raised if possible)",
+              "Beef (grass fed if possible)",
+              "Fish of any sort",
+              "Eggs",
+            ],
+          },
+          {
+            title: "Veggies",
+            list: [
+              "Leafy greens (any of them)",
+              "Squash",
+              "Zucchini",
+              "Celery",
+              "Cucumbers",
+              "Onions",
+              "Peppers",
+              "Cabbage",
+              "Brussels Sprouts",
+              "Eggplant",
+              "Artichokes",
+              "Broccoli",
+              "Asparagus",
+            ],
+          },
+        ],
+      },
+      {
+        title: "DON'T Eat",
+        foodLists: [
+          {
+            title: "Foods",
+            list: [
+              "Sugar",
+              "Processed Oils",
+              "Margarine",
+              "Vegetable Shortening",
+              "Ingredients that list Hydrogenated (fully or partially) Oils",
+              "Processed Meat - hotdogs, chicken nuggets, anything from a box",
+              "Bread, pasta, rice, and other refined carbs",
+              "Beer and mixed drinks",
+              "Honey and syrups",
+              "Juice",
+              "Sugary sodas",
+              "Ketchup, barbecue sauce, and other condiments",
+              "Glazed or honey-baked ham",
+              "Light or low fat margarine",
+              "Dried fruit or trail mix",
+              "Low fat diet foods",
+              "Potatoes and other starchy vegetables",
+              "Sweetened yogurt or dairy",
+            ],
+          },
+        ],
+      },
+    ],
+  },
+};
