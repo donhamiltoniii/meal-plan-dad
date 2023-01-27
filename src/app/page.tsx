baa3af5a1f1fa6ac3779f42359fa93712dc0e58e
Don Hamilton III <donhamiltoniii@gmail.com>
2023-01-27 09:52:25 -0500

    Update tips

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,19 @@
-import { pageData } from "@/data/home-page";
 import { getRecipeLinks } from "@/lib/get-recipe-links";
 import { RecipeLink } from "@/types";
+import fs from "fs";
+import matter from "gray-matter";
+import Markdown from "markdown-to-jsx";
 import Link from "next/link";
 
 export default async function Home() {
-  const { sectionOne, sectionTwo, sectionThree } = pageData;
+  const getPageContent = () => {
+    const folder = "src/data/";
+    const file = `${folder}home-page.mdx`;
+    const fileContent = fs.readFileSync(file, "utf8");
+    const matterData = matter(fileContent);
+
+    return matterData.content;
+  };
 
   const recipeListItems: React.ReactNode[] = getRecipeLinks().map(
     (recipeLink: RecipeLink) => (
@@ -16,52 +25,7 @@ export default async function Home() {
 
   return (
     <main>
-      <section>
-        <h2>{sectionOne.title}</h2>
-        <ul>
-          {sectionOne.list.map((item: string) => (
-            <li key={item}>{item}</li>
-          ))}
-        </ul>
-      </section>
-      <section>
-        <h2>{sectionTwo.title}</h2>
-        <ol>
-          {sectionTwo.list.map((item: string) => (
-            <li key={item}>{item}</li>
-          ))}
-        </ol>
-      </section>
-      <section>
-        <h2>{sectionThree.title}</h2>
-
-        <p>{sectionThree.description}</p>
-
-        {sectionThree.lists.map(
-          (topList: {
-            title: string;
-            foodLists: {
-              title: string;
-              list: string[];
-            }[];
-          }) => (
-            <section key={topList.title}>
-              <h3>{topList.title}</h3>
-
-              {topList.foodLists.map(
-                (list: { title: string; list: string[] }) => (
-                  <ul key={list.title}>
-                    <h4>{list.title}</h4>
-                    {list.list.map((item: string) => (
-                      <li key={item}>{item}</li>
-                    ))}
-                  </ul>
-                )
-              )}
-            </section>
-          )
-        )}
-      </section>
+      <Markdown>{getPageContent()}</Markdown>
       <section>
         <h2>Recipes</h2>
         <nav>
